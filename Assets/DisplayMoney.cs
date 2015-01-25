4445d58c8f88565a0bf94abdaaf759c8ebf3deed
siulkilulki <jurkiewiczdawid@gmail.com>
2015-01-25 15:00:48 +0100

    add

---
+++ Assets/DisplayMoney.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayMoney : MonoBehaviour {
+
+    Text moneytext;
+    public int money;
+    // Use this for initialization
+    void Start () {
+        money = 100;
+      
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        moneytext.text = "Money: $" + money.ToString();
+    }
+}
